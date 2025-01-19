76163e71f7c4070f6cf4f93b6f7833d38e06f36a
Alexander Strauch <strauch.alexander84@gmail.com>
2025-01-19 12:50:49 +0000

    Add user profiles forms

---
+++ profiles/forms.py
@@ -0,0 +1,59 @@
+from django import forms
+from .models import UserProfile
+from django.contrib.auth.models import User
+
+
+class UserProfileForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=30, required=False)
+
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+        fields = ['first_name', 'last_name', 'default_phone_number',
+                 'default_street_address1', 'default_street_address2',
+                 'default_town_or_city', 'default_county',
+                 'default_postcode', 'default_country']
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'default_phone_number': 'Phone number',
+            'default_postcode': 'Postal code',
+            'default_town_or_city': 'Town or city',
+            'default_street_address1': 'Street address',
+            'default_street_address2': 'House number',
+            'default_county': 'County, state or locality',
+        }
+
+        self.fields['first_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+            self.fields[field].label = False
+
+        if self.instance and self.instance.user:
+            self.fields['first_name'].initial = self.instance.user.first_name
+            self.fields['last_name'].initial = self.instance.user.last_name
+
+    def save(self, commit=True):
+        profile = super().save(commit=False)
+        if commit:
+            profile.save()
+            # Update User model fields
+            user = profile.user
+            user.first_name = self.cleaned_data['first_name']
+            user.last_name = self.cleaned_data['last_name']
+            user.save()
+        return profile
