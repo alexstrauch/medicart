71a3fc3adfc3503d68d75083df576ea1cd7e4ac4
Alexander Strauch <strauch.alexander84@gmail.com>
2025-01-20 06:27:55 +0000

    Update marketing views for Mailchimp

--- marketing/views.py
+++ marketing/views.py
@@ -3,9 +3,36 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
+from django.conf import settings
 from .models import NewsletterSubscriber
+import mailchimp_marketing as MailchimpMarketing
+from mailchimp_marketing.api_client import ApiClientError
 
-# Create your views here.
+
+def subscribe_to_mailchimp(email):
+    """
+    Subscribe email to Mailchimp list
+    """
+    try:
+        client = MailchimpMarketing.Client()
+        client.set_config({
+            "api_key": settings.MAILCHIMP_API_KEY,
+            "server": settings.MAILCHIMP_REGION
+        })
+
+        response = client.lists.add_list_member(settings.MAILCHIMP_AUDIENCE_ID, {
+            "email_address": email,
+            "status": "subscribed"
+        })
+        return True, "Successfully subscribed to newsletter!"
+    except ApiClientError as error:
+        error_message = error.text
+        if "Member Exists" in error_message:
+            return False, "You are already subscribed to our newsletter."
+        return False, "An error occurred. Please try again later."
+    except Exception as e:
+        return False, "An error occurred. Please try again later."
+
 
 def newsletter_signup(request):
     """Handle newsletter signup"""
@@ -16,18 +43,14 @@ def newsletter_signup(request):
             # Validate email
             validate_email(email)
             
-            # Try to create subscriber
+            # Try to create subscriber in local database
             subscriber, created = NewsletterSubscriber.objects.get_or_create(
                 email=email
             )
             
-            if created:
-                message = 'Thank you for subscribing to our newsletter!'
-                success = True
-            else:
-                message = 'You are already subscribed to our newsletter.'
-                success = False
-                
+            # Subscribe to Mailchimp
+            success, message = subscribe_to_mailchimp(email)
+            
             if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
                 return JsonResponse({
                     'success': success,
@@ -49,7 +72,7 @@ def newsletter_signup(request):
             else:
                 messages.error(request, message)
         except Exception as e:
-            message = 'An error occurred. Please try again.'
+            message = 'An error occurred. Please try again later.'
             if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
                 return JsonResponse({
                     'success': False,
