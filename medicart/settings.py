8438318cf7eb034ec31856287e58dcb393190252
Alexander Strauch <strauch.alexander84@gmail.com>
2025-01-20 10:26:39 +0000

    Update email settings

--- medicart/settings.py
+++ medicart/settings.py
@@ -130,9 +130,11 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
+    EMAIL_USE_SSL = False
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    SERVER_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # AllAuth settings
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
