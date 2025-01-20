9fa4a0ba11def26efdbf91c8bc1233bbe94b9eaa
Alexander Strauch <strauch.alexander84@gmail.com>
2025-01-20 08:25:42 +0000

    Fix DEBUG setting evaluation

--- medicart/settings.py
+++ medicart/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
