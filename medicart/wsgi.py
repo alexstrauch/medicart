fd340f5c48400033ed7c4b3671931de47ea3e757
alexstrauch <161021285+alexstrauch@users.noreply.github.com>
2025-03-02 15:43:30 +0100

    Update docstrings for wsgi.py

--- medicart/wsgi.py
+++ medicart/wsgi.py
@@ -1,16 +1,23 @@
 """
-WSGI config for medicart project.
+WSGI configuration for the MediCart project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+This module configures the Web Server Gateway Interface (WSGI)
+for production deployment. It exposes the WSGI callable as a
+module-level variable named 'application'.
 
-For more information on this file, see
+WSGI is the standard interface between Python web applications
+and web servers like Apache or Nginx. This configuration enables
+the project to be deployed on any WSGI-compliant server.
+
+For more information, see:
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set the Django settings module path
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medicart.settings')
 
+# Initialize WSGI application
 application = get_wsgi_application()
